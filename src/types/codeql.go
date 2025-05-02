d253bfc1d3a892363a32769281e980a94894cdb2
Cédric HERZOG <cedric.herzog@codeclarity.io>
2025-05-02 10:40:12 +0200

    Store location of the codeql result in DB

--- src/types/codeql.go
+++ src/types/codeql.go
@@ -9,11 +9,34 @@ type Run struct {
 }
 
 type Result struct {
-	RuleId    string  `json:"ruleId"`
-	RuleIndex int     `json:"ruleIndex"`
-	Message   Message `json:"message"`
+	RuleId    string     `json:"ruleId"`
+	RuleIndex int        `json:"ruleIndex"`
+	Message   Message    `json:"message"`
+	Locations []Location `json:"locations"`
 }
 
 type Message struct {
 	Text string `json:"text"`
+}
+
+type Location struct {
+	PhysicalLocation PhysicalLocation `json:"physicalLocation"`
+}
+
+type PhysicalLocation struct {
+	ArtifactLocation ArtifactLocation `json:"artifactLocation"`
+	Region           Region           `json:"region"`
+}
+
+type ArtifactLocation struct {
+	URI       string `json:"uri"`
+	URIBaseId string `json:"uriBaseId"`
+	Index     int    `json:"index"`
+}
+
+type Region struct {
+	StartLine   int `json:"startLine"`
+	EndLine     int `json:"endLine"`
+	StartColumn int `json:"startColumn"`
+	EndColumn   int `json:"endColumn"`
 }
