7bd1c8dc5f210f6f17a5b186e6a60188b2e6e7a4
CedricHerzog <CedricHerzog@users.noreply.github.com>
2025-08-22 23:37:30 +0200

    Refactor CodeQL plugin: update dependencies and replace ecosystem with boilerplates

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 	plugin "github.com/CodeClarityCE/plugin-codeql/src"
 	output "github.com/CodeClarityCE/plugin-codeql/src/types"
-	"github.com/CodeClarityCE/utility-types/ecosystem"
+	"github.com/CodeClarityCE/utility-types/boilerplates"
 	types_amqp "github.com/CodeClarityCE/utility-types/amqp"
 	codeclarity "github.com/CodeClarityCE/utility-types/codeclarity_db"
 	plugin_db "github.com/CodeClarityCE/utility-types/plugin_db"
@@ -19,7 +19,7 @@ type CodeQLAnalysisHandler struct{}
 
 // StartAnalysis implements the AnalysisHandler interface
 func (h *CodeQLAnalysisHandler) StartAnalysis(
-	databases *ecosystem.PluginDatabases,
+	databases *boilerplates.PluginDatabases,
 	dispatcherMessage types_amqp.DispatcherPluginMessage,
 	config plugin_db.Plugin,
 	analysisDoc codeclarity.Analysis,
@@ -29,7 +29,7 @@ func (h *CodeQLAnalysisHandler) StartAnalysis(
 
 // main is the entry point of the program.
 func main() {
-	pluginBase, err := ecosystem.NewPluginBase()
+	pluginBase, err := boilerplates.CreatePluginBase()
 	if err != nil {
 		log.Fatalf("Failed to initialize plugin base: %v", err)
 	}
@@ -43,7 +43,7 @@ func main() {
 	}
 }
 
-func startAnalysis(databases *ecosystem.PluginDatabases, dispatcherMessage types_amqp.DispatcherPluginMessage, config plugin_db.Plugin, analysis_document codeclarity.Analysis) (map[string]any, codeclarity.AnalysisStatus, error) {
+func startAnalysis(databases *boilerplates.PluginDatabases, dispatcherMessage types_amqp.DispatcherPluginMessage, config plugin_db.Plugin, analysis_document codeclarity.Analysis) (map[string]any, codeclarity.AnalysisStatus, error) {
 	// Get analysis config
 	messageData := analysis_document.Config[config.Name].(map[string]any)
 
