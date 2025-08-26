cd813e2ad656a3d6ffa484cf552cdded44b27bee
CedricHerzog <CedricHerzog@users.noreply.github.com>
2025-08-26 11:38:19 +0200

    Update dependencies and refactor CodeQL plugin (#12)
    * Update golang.org/x/crypto and golang.org/x/sys to latest versions in go.mod and go.sum
    
    * Refactor CodeQL plugin: streamline analysis handler and remove unused utilities
    
    * Refactor CodeQL plugin: update dependencies and replace ecosystem with boilerplates
    
    * Update go.work.sum: add new dependencies and update existing ones
    
    * Remove pg_bouncer service dependency from docker-compose.yaml
    
    * Format
    
    * Format
    
    * Update go.mod and go.sum: add new indirect dependencies and remove unused ones
    
    * Update dependencies
    
    * Bump version to v0.0.14-alpha in config.json

--- main.go
+++ main.go
@@ -2,80 +2,48 @@ package main
 
 import (
 	"context"
-	"database/sql"
 	"log"
 	"os"
 	"time"
 
 	plugin "github.com/CodeClarityCE/plugin-codeql/src"
 	output "github.com/CodeClarityCE/plugin-codeql/src/types"
-	amqp_helper "github.com/CodeClarityCE/utility-amqp-helper"
-	dbhelper "github.com/CodeClarityCE/utility-dbhelper/helper"
+	"github.com/CodeClarityCE/utility-boilerplates"
 	types_amqp "github.com/CodeClarityCE/utility-types/amqp"
 	codeclarity "github.com/CodeClarityCE/utility-types/codeclarity_db"
 	plugin_db "github.com/CodeClarityCE/utility-types/plugin_db"
-	"github.com/uptrace/bun"
-	"github.com/uptrace/bun/dialect/pgdialect"
-	"github.com/uptrace/bun/driver/pgdriver"
 )
 
-// Define the arguments you want to pass to the callback function
-type Arguments struct {
-	codeclarity *bun.DB
-	knowledge   *bun.DB
+// CodeQLAnalysisHandler implements the AnalysisHandler interface
+type CodeQLAnalysisHandler struct{}
+
+// StartAnalysis implements the AnalysisHandler interface
+func (h *CodeQLAnalysisHandler) StartAnalysis(
+	databases *boilerplates.PluginDatabases,
+	dispatcherMessage types_amqp.DispatcherPluginMessage,
+	config plugin_db.Plugin,
+	analysisDoc codeclarity.Analysis,
+) (map[string]any, codeclarity.AnalysisStatus, error) {
+	return startAnalysis(databases, dispatcherMessage, config, analysisDoc)
 }
 
 // main is the entry point of the program.
-// It reads the configuration, initializes the necessary databases and graph,
-// and starts listening on the queue.
 func main() {
-	config, err := readConfig()
+	pluginBase, err := boilerplates.CreatePluginBase()
 	if err != nil {
-		log.Printf("%v", err)
-		return
+		log.Fatalf("Failed to initialize plugin base: %v", err)
 	}
+	defer pluginBase.Close()
 
-	host := os.Getenv("PG_DB_HOST")
-	if host == "" {
-		log.Printf("PG_DB_HOST is not set")
-		return
+	// Start the plugin with our analysis handler
+	handler := &CodeQLAnalysisHandler{}
+	err = pluginBase.Listen(handler)
+	if err != nil {
+		log.Fatalf("Failed to start plugin: %v", err)
 	}
-	port := os.Getenv("PG_DB_PORT")
-	if port == "" {
-		log.Printf("PG_DB_PORT is not set")
-		return
-	}
-	user := os.Getenv("PG_DB_USER")
-	if user == "" {
-		log.Printf("PG_DB_USER is not set")
-		return
-	}
-	password := os.Getenv("PG_DB_PASSWORD")
-	if password == "" {
-		log.Printf("PG_DB_PASSWORD is not set")
-		return
-	}
-
-	dsn_knowledge := "postgres://" + user + ":" + password + "@" + host + ":" + port + "/" + dbhelper.Config.Database.Knowledge + "?sslmode=disable"
-	sqldb_knowledge := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn_knowledge)))
-	db_knowledge := bun.NewDB(sqldb_knowledge, pgdialect.New())
-	defer db_knowledge.Close()
-
-	dsn := "postgres://" + user + ":" + password + "@" + host + ":" + port + "/" + dbhelper.Config.Database.Results + "?sslmode=disable"
-	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
-	db_codeclarity := bun.NewDB(sqldb, pgdialect.New())
-	defer db_codeclarity.Close()
-
-	args := Arguments{
-		codeclarity: db_codeclarity,
-		knowledge:   db_knowledge,
-	}
-
-	// Start listening on the queue
-	amqp_helper.Listen("dispatcher_"+config.Name, callback, args, config)
 }
 
-func startAnalysis(args Arguments, dispatcherMessage types_amqp.DispatcherPluginMessage, config plugin_db.Plugin, analysis_document codeclarity.Analysis) (map[string]any, codeclarity.AnalysisStatus, error) {
+func startAnalysis(databases *boilerplates.PluginDatabases, dispatcherMessage types_amqp.DispatcherPluginMessage, config plugin_db.Plugin, analysis_document codeclarity.Analysis) (map[string]any, codeclarity.AnalysisStatus, error) {
 	// Get analysis config
 	messageData := analysis_document.Config[config.Name].(map[string]any)
 
@@ -97,7 +65,7 @@ func startAnalysis(args Arguments, dispatcherMessage types_amqp.DispatcherPlugin
 		Plugin:     config.Name,
 		CreatedOn:  time.Now(),
 	}
-	_, err := args.codeclarity.NewInsert().Model(&result).Exec(context.Background())
+	_, err := databases.Codeclarity.NewInsert().Model(&result).Exec(context.Background())
 	if err != nil {
 		panic(err)
 	}
